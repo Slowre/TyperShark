4c8d5e9549ef2bb59e5d31398662eeaa8f6291a2
Jefferson Del Barco <jdel@espol.edu.ec>
2016-08-23 22:21:57 -0500

    Arreglo y modificaciones
    Se arreglo la direccion del tiburon negro y las vidas disminuyen

---
+++ 23-08-2016_2/src/Recursos/HiloBuceador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recursos;
+
+import Personajes.Buceador;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author Flores
+ */
+public class HiloBuceador extends Thread {
+
+    Buceador buceador;
+
+    public HiloBuceador(Buceador buceador) {
+        this.buceador = buceador;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+
+            this.buceador.getImageView().setLayoutX(this.buceador.getPuntosRound() * 4 / 5);
+            if (this.buceador.getVidas() == 0) {
+                break;
+            }
+        }
+
+    }
+
+}
